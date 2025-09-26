141d7e830244ee2787a84e288283d42f4a146d69
Aswin MS <114893969+Aswin-MS@users.noreply.github.com>
2025-09-26 14:05:03 +0530

    25sept

--- Pandas/missing.py
+++ Pandas/missing.py
@@ -11,13 +11,35 @@ print(df.isna().sum())
 # print(df)
 # df['Date'].fillna('2025/12/25',inplace=True)
 # df.fillna({'Date':'123'},inplace=True)
-print(df['Calories'].unique())
-m=df['Calories'].mean()
-print(m)
-df['Calories'].fillna(m,inplace=True)
+# print(df['Calories'].unique())
+# m=df['Calories'].mean()
+# print(m)
+# df['Calories'].fillna(m,inplace=True)
+# print(df)
+# print('*'*100)
+# d=df['Date'].mode()[0]
+# print(d)
+# df['Date'].fillna(d,inplace=True)
+# print(df)
+
+#------------------>
+#25 SEPT
+#------------------>
+
+# df.dropna(inplace=True,ignore_index=True)
+# df.dropna(subset=['Date'],inplace=True,ignore_index=True)
+# df.dropna(subset=['Calories'],inplace=True,ignore_index=True)
+# print(df)
+x=df['Duration'].mode()[0]
+print(x)
+df.loc[7,'Duration']=x
 print(df)
 print('*'*100)
-d=df['Date'].mode()[0]
-print(d)
-df['Date'].fillna(d,inplace=True)
+#assume calories above 400 is wrong data
+#use mean to fill that data
+c=df['Calories'].mean()
+print(c)
+df.loc[df['Calories']>400,'Calories']=c
 print(df)
+for i in df:
+    print(i)
